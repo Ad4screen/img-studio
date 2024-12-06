3b19d78afac285ac8060f9625475856810b45b01
Thomas Raveneau <traveneau@ad4screen.com>
2024-12-06 11:58:23 +0100

    [DEPLOY] - 4

--- app/(studio)/edit/page.tsx
+++ app/(studio)/edit/page.tsx
@@ -10,8 +10,7 @@ import { useAppContext } from '../../context/app-context'
 import { Typography } from '@mui/material'
 
 import theme from '../../theme'
-import EditForm from '../../ui/edit-components/EditForm'
-// import EditForm from '@/app/ui/edit-components/EditForm'
+import EditForm from '@/app/ui/edit-components/EditForm'
 import { redirect } from 'next/navigation'
 const { palette } = theme
 
